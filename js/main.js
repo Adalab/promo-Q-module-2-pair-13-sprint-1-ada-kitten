2f669e7648ac491bb4a7f790e3edefd3070c975c
miralmag <miriam.almagro@gmail.com>
2022-06-25 09:16:22 +0200

    add variables in js

--- js/main.js
+++ js/main.js
@@ -2,4 +2,63 @@
 
 const form = document.querySelector('.js-new-form');
 
-form.classList.remove("collapsed");
+// form.classList.remove("collapsed");
+
+const kittenList = document.querySelector (".js_list");
+let kittenImage = 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg';
+let kittenName = 'Anastacio';
+let kittenDesc = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
+let kittenRace = 'British Shorthair';
+const kittenOne = `<li class="card">
+<article>
+  <img
+    class="card_img"
+    src= ${kittenImage}
+    alt="gatito"
+  />
+  <h3 class="card_title">${kittenName.toUpperCase()}</h3>
+  <h4 class="card_race">${kittenRace}</h4>
+  <p class="card_description">
+    ${kittenDesc}
+  </p>
+</article>
+</li>`;
+
+kittenImage = 'https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg';
+kittenName = 'Fiona';
+
+const kittenTwo = `<li class="card">
+<article>
+  <img
+    class="card_img"
+    src= ${kittenImage}
+    alt="gatito"
+  />
+  <h3 class="card_title">${kittenName.toUpperCase()}</h3>
+  <h4 class="card_race">${kittenRace}</h4>
+  <p class="card_description">
+    ${kittenDesc}
+  </p>
+</article>
+</li>`;
+
+kittenImage = 'https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg';
+kittenName = 'Cielo';
+
+const kittenThree = `<li class="card">
+<article>
+  <img
+    class="card_img"
+    src= ${kittenImage}
+    alt="gatito"
+  />
+  <h3 class="card_title">${kittenName.toUpperCase()}</h3>
+  <h4 class="card_race">${kittenRace}</h4>
+  <p class="card_description">
+    ${kittenDesc}
+  </p>
+</article>
+</li>`;
+
+
+kittenList.innerHTML = kittenOne + kittenTwo + kittenThree;
